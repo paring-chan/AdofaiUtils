47a2622f9373d571aa126bc5f04511ed1f063faa
파랑이 <parangee9706@gmail.com>
2021-07-09 16:03:55 +0900

    와 샌즈

---
+++ AdofaiUtils2/Main.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace AdofaiUtils2
+{
+    [BepInPlugin("dev.pikokr.plugins.adofaiutils2", "AdofaiUtils2", "0.0.1")]
+    public class Main : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Debug.Log("sans!");
+        }
+    }
+}
