dfc4c002ba3bc7063eb41d3155a2cab525db93b2
파랑이 <parangee9706@gmail.com>
2021-07-15 15:32:17 +0900

    셋업

---
+++ AdofaiUtils.Core/CoreModule.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityModManagerNet;
+
+namespace AdofaiUtils.Core
+{
+    internal static class CoreModule
+    {
+        private static UnityModManager.ModEntry _modEntry;
+        
+        private static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            return true;
+        }
+
+        private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            Debug.Log(value);
+            _modEntry = modEntry;
+            return true;
+        }
+    }
+}
