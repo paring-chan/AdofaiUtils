e3097c5970a07bf84693bc68284f02db852caf45
파랑이 <parangee9706@gmail.com>
2021-07-15 13:09:56 +0900

    thunderkit을 이용해 프로젝트 셋업

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+
+[BepInPlugin("io.github.pikokr.adofaiutils2", "AdofaiUtils2", "0.0.1")]
+public class Main : BaseUnityPlugin
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- copy.sh
+++
@@ -1,7 +0,0 @@
-export OUT_DIR="/mnt/c/Program Files (x86)/Steam/steamapps/common/A Dance of Fire and Ice/BepInEx/plugins/AdofaiUtils"
-
-rm -rf "$OUT_DIR"
-
-mkdir -p "$OUT_DIR"
-
-cp AdofaiUtils2/bin/Debug/* "$OUT_DIR"
