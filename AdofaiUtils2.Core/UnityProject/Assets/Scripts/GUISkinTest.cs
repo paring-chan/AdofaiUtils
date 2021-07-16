763782343639f0e658eed53678d2c83394ae1182
파랑이 <parangee9706@gmail.com>
2021-07-16 15:18:29 +0900

    GUI 스킨 만듬

---
+++ AdofaiUtils2.Core/UnityProject/Assets/Scripts/GUISkinTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+public class GUISkinTest : MonoBehaviour
+{
+    public GUISkin skin;
+
+    private Vector2 _pos = new Vector2(0, 0);
+    
+    private void OnGUI()
+    {
+        GUI.skin = skin;
+        
+        GUILayout.BeginArea(new Rect(50,50, Screen.width-100,Screen.height-100), "와아아", GUI.skin.window);
+
+        // GUI.Button(new Rect(50,50,300, 50), "버튼");
+
+        _pos = GUILayout.BeginScrollView(_pos);
+
+        GUILayout.Button("와아아");
+        GUILayout.Button("와아아");
+        GUILayout.Button("와아아");
+        GUILayout.Button("와아아");
+        GUILayout.Button("와아아");
+        GUILayout.Button("와아아");
+        GUILayout.Button("와아아");
+        GUILayout.Button("와아아");
+        GUILayout.Button("와아아");
+        GUILayout.Button("와아아");
+        GUILayout.Button("와아아");
+        GUILayout.Button("와아아");
+        GUILayout.Button("와아아");
+        GUILayout.Button("와아아");
+        GUILayout.Button("와아아");
+        GUILayout.Button("와아아");
+        
+        GUILayout.EndScrollView();
+        
+        GUILayout.EndArea();
+    }
+}
