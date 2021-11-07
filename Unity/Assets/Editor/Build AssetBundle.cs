99de765bee8b3895b4f7f4d9305859e56f5505d6
pikokr <parangee9706@gmail.com>
2021-11-07 15:02:19 +0900

    init unity project

---
+++ Unity/Assets/Editor/Build AssetBundle.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using UnityEditor;
+
+public static class BuildAssetBundle
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None,
+            EditorUserBuildSettings.activeBuildTarget);
+    }
+}
