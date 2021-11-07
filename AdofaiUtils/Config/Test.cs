4a83e7b9303d9c9e6df50c0cffb0dc25e93f3630
pikokr <parangee9706@gmail.com>
2021-11-07 20:07:25 +0900

    enum settings

--- AdofaiUtils/Config/Test.cs
+++ AdofaiUtils/Config/Test.cs
@@ -1,4 +1,6 @@
-﻿using AdofaiUtils.Attribute;
+﻿using System;
+using System.Linq;
+using AdofaiUtils.Attribute;
 using AdofaiUtils.Utils;
 using MelonLoader;
 
@@ -11,7 +13,7 @@ namespace AdofaiUtils.Config
         {
             RemoveArrowButtons(btn);
             btn.type = "Action";
-            btn.label.text = "와! 샌즈!";
+            btn.label.text = "와!!!!!!!!!!!!!!!!";
         }
 
         public override void OnInteract(PauseSettingButton setting, SettingsMenu.Interaction action)
@@ -28,7 +30,7 @@ namespace AdofaiUtils.Config
         public override void Init(PauseSettingButton btn)
         {
             btn.type = "Int";
-            btn.label.text = "와! 샌즈!!!!!";
+            btn.label.text = "와!!!";
             btn.minInt = 1;
             btn.maxInt = 10;
             btn.valueLabel.text = value.ToString();
@@ -37,6 +39,31 @@ namespace AdofaiUtils.Config
         public override void OnInteract(PauseSettingButton setting, SettingsMenu.Interaction action)
         {
             IntCallback(setting, action, ref value);
+        }
+    }
+
+    [Config]
+    internal class Test3 : ConfigItem
+    {
+        private TestEnum value = TestEnum.TEST1;
+
+        enum TestEnum
+        {
+            TEST1,
+            TEST2,
+            TEST3
+        }
+
+        public override void Init(PauseSettingButton btn)
+        {
+            btn.type = $"Enum:{typeof(TestEnum).FullName}";
+            btn.label.text = "와!!!!!!!";
+            btn.valueLabel.text = value.ToString();
+        }
+
+        public override void OnInteract(PauseSettingButton setting, SettingsMenu.Interaction action)
+        {
+            EnumCallback(setting, action, ref value);
         }
     }
 }
--- AdofaiUtils/Utils/ConfigItem.cs
+++ AdofaiUtils/Utils/ConfigItem.cs
@@ -1,4 +1,6 @@
-﻿namespace AdofaiUtils.Utils
+﻿using System;
+
+namespace AdofaiUtils.Utils
 {
     internal class ConfigItem
     {
@@ -20,15 +22,55 @@
         {
             if (action == SettingsMenu.Interaction.Increment && value < setting.maxInt)
             {
+                setting.PlayArrowAnimation(true);
+
+                scrConductor.instance.PlaySfx(2, ignoreListenerPause: true);
+
                 value++;
                 setting.valueLabel.text = value.ToString();
             }
 
             if (action == SettingsMenu.Interaction.Decrement && value > setting.minInt)
             {
+                setting.PlayArrowAnimation(false);
+
+                scrConductor.instance.PlaySfx(2, ignoreListenerPause: true);
+
                 value--;
                 setting.valueLabel.text = value.ToString();
             }
+        }
+
+        protected void EnumCallback<T>(PauseSettingButton setting, SettingsMenu.Interaction action, ref T value)
+            where T : Enum
+        {
+            var t = typeof(T);
+            var names = Enum.GetNames(t);
+            var idx = Array.IndexOf(names, Enum.GetName(t, value));
+            if (action == SettingsMenu.Interaction.Increment) setting.PlayArrowAnimation(true);
+            if (action == SettingsMenu.Interaction.Decrement) setting.PlayArrowAnimation(false);
+      
+            scrConductor.instance.PlaySfx(2, ignoreListenerPause: true);
+
+            if (action == SettingsMenu.Interaction.Increment && names.Length - 1 > idx)
+            {
+                idx++;
+            }
+            else if (action == SettingsMenu.Interaction.Increment && names.Length - 1 == idx)
+            {
+                idx = 0;
+            }
+            else if (action == SettingsMenu.Interaction.Decrement && 0 < idx)
+            {
+                idx--;
+            }
+            else if (action == SettingsMenu.Interaction.Decrement && idx == 0)
+            {
+                idx = names.Length - 1;
+            }
+
+            value = (T) Enum.ToObject(t, idx);
+            setting.valueLabel.text = value.ToString();
         }
     }
 }
